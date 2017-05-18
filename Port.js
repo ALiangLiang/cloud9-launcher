153a300e3e522c0b1d890c986f623e589b5a9646
ALiangLiang <david27072183@gmail.com>
2017-05-19 01:12:23 +0800

    Beautify and remove useless dependencies

--- Port.js
+++ Port.js
@@ -1,37 +1,37 @@
 class Port {
-	constructor(port, project) {
-		this._port = port;
-		this._project = project;
-	}
-	
-	free() {
-	    this._project = void 0;
-	}
-	
-	set project(project) {
-		this._project = project;
-	}
-	
-	get project() {
-		return this._project;
-	}
-	
-	get isFree() {
-		return !this._project;
-	}
-	
-	get number() {
-		return this._port;
-	}
-	
-	set project(project) {
-		// two way assign
-		if(!project.port) {
-			this.project = project;
-			project.port = this;
-		} else
-			console.log('Ths project is running on port ' + project.port.number);
-	}
+    constructor(port, project) {
+        this._port = port;
+        this._project = project;
+    }
+
+    free() {
+        this._project = void 0;
+    }
+
+    set project(project) {
+        this._project = project;
+    }
+
+    get project() {
+        return this._project;
+    }
+
+    get isFree() {
+        return !this._project;
+    }
+
+    get number() {
+        return this._port;
+    }
+
+    set project(project) {
+        // two way assign
+        if (!project.port) {
+            this.project = project;
+            project.port = this;
+        } else
+            console.log('Ths project is running on port ' + project.port.number);
+    }
 }
 
 module.exports = Port;
--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-### Cloud9 Launcher
+## ️️☁️Cloud9 Launcher☁️ (In dev)
 
 It's a Nodejs application allows you manage your Cloud9 IDE workflows on your own server without the need of a terminal.
 
@@ -17,3 +17,7 @@ cp config/default-0.json5 config/local-0.json5
 vim config/local-0.json5 # fill this configure file
 npm start
 ```
+
+### About
+
+This project is reference [c9ui](https://github.com/orditeck/c9ui). Coz I don't want to prepare environment about PHP 😛. BTW, I am not English-speaker. So if you discover some grammar error, please help me fixed and PR. lol
